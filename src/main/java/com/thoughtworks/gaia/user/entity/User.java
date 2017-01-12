2320614f9b8b158d54d4cac501db5f719f052028
syu <syu@thoughtworks.com>
2017-01-12 23:48:54 +0800

    Constructed User and User Model.

--- src/main/java/com/thoughtworks/gaia/user/entity/User.java
+++ src/main/java/com/thoughtworks/gaia/user/entity/User.java
@@ -3,18 +3,48 @@ package com.thoughtworks.gaia.user.entity;
 import java.util.Date;
 
 public class User {
-    private Long id;
+    private short id;
+
+    private String email;
+
+    private String password;
 
     private String name;
 
+    private boolean gender;
+
+    private String tel;
+
+    private String school;
+
+    private String major;
+
+    private short userTypeId;
+
     private Date timeCreated;
 
-    public Long getId() {
+    public short getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(short id) {
         this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getName() {
@@ -23,6 +53,46 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public short getUserTypeId() {
+        return userTypeId;
+    }
+
+    public void setUserTypeId(short userTypeId) {
+        this.userTypeId = userTypeId;
     }
 
     public Date getTimeCreated() {
--- src/main/java/com/thoughtworks/gaia/user/model/UserModel.java
+++ src/main/java/com/thoughtworks/gaia/user/model/UserModel.java
@@ -8,13 +8,50 @@ import javax.persistence.Table;
 import java.util.Date;
 
 @Entity
-@Table(name = "PRODUCT")
+@Table(name = "user")
 public class UserModel extends IdBaseModel {
-    @Column(name = "name", nullable = false, length = 64)
+    @Column(name = "email", nullable = false, length = 64)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 24)
+    private String password;
+
+    @Column(name = "name", nullable = true, length = 64)
     private String name;
+
+    @Column(name = "gender", nullable = true)
+    private boolean gender;
+
+    @Column(name = "tel", nullable = true, length = 11)
+    private String tel;
+
+    @Column(name = "school", nullable = true, length = 64)
+    private String school;
+
+    @Column(name = "major", nullable = true, length = 64)
+    private String major;
+
+    @Column(name = "user_type_id", nullable = false)
+    private short user_type_id;
 
     @Column(name = "time_created", nullable = false, updatable = false)
     private Date timeCreated;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getName() {
         return name;
@@ -22,6 +59,46 @@ public class UserModel extends IdBaseModel {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public short getUserTypeId() {
+        return user_type_id;
+    }
+
+    public void setUserTypeId(short userTypeId) {
+        this.user_type_id = userTypeId;
     }
 
     public Date getTimeCreated() {
