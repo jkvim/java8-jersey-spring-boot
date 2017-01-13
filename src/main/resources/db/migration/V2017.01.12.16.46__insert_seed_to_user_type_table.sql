5cf8380114faf416a7a59ec5b1d130eae95a668e
Jie Meng <jiemeng@thoughtworks.com>
2017-01-13 11:19:56 +0800

    Pick out seed sql from create_table sql.

---
+++ src/main/resources/db/migration/V2017.01.12.16.46__insert_seed_to_user_type_table.sql
@@ -0,0 +1,2 @@
+INSERT INTO user_type (`id`, `type`) VALUES ('1', 'student');
+INSERT INTO user_type (`id`, `type`) VALUES ('2', 'admin');
---
+++ src/main/resources/db/migration/V2017.01.12.17.40__insert_seed_to_question_type_table.sql
@@ -0,0 +1,2 @@
+INSERT INTO question_type (`id`, `type`) VALUES ('1', 'logic');
+INSERT INTO question_type (`id`, `type`) VALUES ('2', 'programmable');
